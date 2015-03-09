837daa8810e32b65b7a23b128571ab2ec74fba74
pal <pal@zuehlke.com>
2015-03-09 15:37:30 +0100

    Added tests for Future.Subscribe(Action, Action)

---
+++ Futures/FutureTests/Subscription.cs
@@ -0,0 +1,40 @@
+﻿namespace FutureTests
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Futures;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Subscription
+    {
+        [TestMethod]
+        public void SubscribingExecutesOnNext()
+        {
+            object received = null;
+            Exception ex = null;
+
+            var sut = Future.Return(10);
+            sut.Subscribe(x => received = x, x => ex = x);
+
+            received.Should().Be(10);
+            ex.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void SubscribingExecutesOnError()
+        {
+            object received = null;
+            Exception ex = null;
+
+            var sut = Future.Fail<int>(new NotImplementedException());
+            sut.Subscribe(x => received = x, x => ex = x);
+
+            received.Should().BeNull();
+            ex.Should().BeOfType<NotImplementedException>();
+        }    
+    }
+}
