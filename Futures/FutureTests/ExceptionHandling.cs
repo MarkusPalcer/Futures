e5165625ca350fa2f8a4d958b94662167453711a
pal <pal@zuehlke.com>
2015-03-10 17:19:54 +0100

    Added IFuture<T>.Retry extensions

--- Futures/FutureTests/ExceptionHandling.cs
+++ Futures/FutureTests/ExceptionHandling.cs
@@ -7,6 +7,7 @@
 
     using Futures;
 
+    using Microsoft.Reactive.Testing;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using Notification = Futures.Notification;
@@ -162,6 +163,173 @@
             future.SetError(new InvalidOperationException());
 
             recorder.Events.Should().Equal(Notification.OnDone(), Notification.OnError<Unit>(ex), Notification.OnDone(), Notification.OnError<Unit>(ex));
+        }
+
+        [TestMethod]
+        public void RetryingWithVariableDelay()
+        {
+            var delays = new[]
+                             {
+                                 TimeSpan.FromMilliseconds(1), TimeSpan.FromMilliseconds(2), TimeSpan.FromMilliseconds(3)
+                             };
+
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<Unit>();
+            var recorder = new TestObserver<Unit>();
+            var ex = new NotImplementedException();
+
+            var sut = future.Retry(delays, scheduler);
+
+            sut.Subscribe(recorder);
+
+            future.SetResult(Unit.Default);
+            recorder.Events.Should().Equal(Notification.OnDone());
+
+
+            recorder = new TestObserver<Unit>();
+            sut.Subscribe(recorder);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(ex);
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(2).Ticks);
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().Equal(Notification.OnError<Unit>(ex));
+        }
+
+        [TestMethod]
+        public void RetryingWithFixedDelay()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<Unit>();
+            var recorder = new TestObserver<Unit>();
+            var ex = new NotImplementedException();
+
+            var sut = future.Retry(TimeSpan.FromMilliseconds(2), 3, scheduler);
+
+            sut.Subscribe(recorder);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(ex);
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().Equal(Notification.OnError<Unit>(ex));            
+        }
+
+        [TestMethod]
+        public void RetryingOnceWithDelay()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<Unit>();
+            var recorder = new TestObserver<Unit>();
+            var ex = new NotImplementedException();
+
+            var sut = future.Retry(TimeSpan.FromMilliseconds(2), scheduler);
+
+            sut.Subscribe(recorder);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(ex);
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().Equal(Notification.OnError<Unit>(ex));   
+        }
+
+        [TestMethod]
+        public void RetryingMultipleTimesImmediately()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<Unit>();
+            var recorder = new TestObserver<Unit>();
+            var ex = new NotImplementedException();
+
+            var sut = future.Retry(2, scheduler);
+            sut.Subscribe(recorder);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(ex);
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().Equal(Notification.OnError<Unit>(ex));
+        }
+
+        [TestMethod]
+        public void RetryingOnceImmediately()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<Unit>();
+            var recorder = new TestObserver<Unit>();
+            var ex = new NotImplementedException();
+
+            var sut = future.Retry(scheduler);
+            sut.Subscribe(recorder);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(ex);
+            recorder.Events.Should().BeEmpty();
+            future.Observers.Should().HaveCount(0);
+            scheduler.AdvanceBy(TimeSpan.FromMilliseconds(1).Ticks);
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(new InvalidOperationException());
+            recorder.Events.Should().Equal(Notification.OnError<Unit>(ex));
         }
     }
 }
--- Futures/Futures/Extensions/ExceptionHandling.cs
+++ Futures/Futures/Extensions/ExceptionHandling.cs
@@ -1,6 +1,9 @@
 ﻿namespace Futures
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reactive.Concurrency;
     using System.Reactive.Disposables;
 
     public static partial class Future
@@ -79,6 +82,67 @@
         public static IFuture<T> Recover<T, TEx>(this IFuture<T> source, IFuture<T> recovery) where TEx : Exception
         {
             return source.Recover<T, TEx>(() => recovery);
-        } 
+        }
+
+        public static IFuture<T> Retry<T>(this IFuture<T> source, IEnumerable<TimeSpan> delays, IScheduler scheduler = null)
+        {
+            scheduler = scheduler ?? Scheduler.Default;
+
+            return Create<T>(
+                o =>
+                    {
+                        var retryQueue = new Queue<TimeSpan>(delays);
+                        var b = new BooleanDisposable();
+                        var m = new MultipleAssignmentDisposable();
+
+                        Action<Exception> handle = null;
+                        handle = ex =>
+                            {
+                                if (retryQueue.Count == 0)
+                                {
+                                    o.OnError(ex);
+                                }
+                                else
+                                {
+                                    if (b.IsDisposed)
+                                    {
+                                        return;
+                                    }
+
+                                    m.Disposable.Dispose();
+                                    m.Disposable = scheduler.Schedule(
+                                        retryQueue.Dequeue(),
+                                        () =>
+                                            {
+                                                m.Disposable = source.Subscribe(o.OnDone, _ => handle(ex));
+                                            });
+                                }
+                            };
+
+                        m.Disposable = source.Subscribe(o.OnDone, handle);
+
+                        return new CompositeDisposable(b, m);
+                    });
+        }
+
+        public static IFuture<T> Retry<T>(this IFuture<T> source, TimeSpan delay, int count, IScheduler scheduler = null)
+        {
+            return source.Retry(Enumerable.Repeat(delay, count), scheduler);
+        }
+
+        public static IFuture<T> Retry<T>(this IFuture<T> source, TimeSpan delay, IScheduler scheduler = null)
+        {
+            return source.Retry(new[] { delay }, scheduler);
+        }
+
+        public static IFuture<T> Retry<T>(this IFuture<T> source, int count, IScheduler scheduler = null)
+        {
+            return source.Retry(Enumerable.Repeat(TimeSpan.Zero, count), scheduler);
+        }
+
+        public static IFuture<T> Retry<T>(this IFuture<T> source, IScheduler scheduler = null)
+        {
+            return source.Retry(new[] { TimeSpan.Zero }, scheduler);
+        }
     }
 }
