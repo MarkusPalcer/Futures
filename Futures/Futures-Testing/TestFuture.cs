39e12ad71ce4d1e26363b314f1212e4d5f143319
pal <pal@zuehlke.com>
2015-03-11 13:28:27 +0100

    Moved test implementations into Futures-Testing project (which will be available as its own package)

--- Futures/Futures-Testing/TestFuture.cs
+++ Futures/Futures-Testing/TestFuture.cs
@@ -1,34 +1,32 @@
-﻿namespace FutureTests
+﻿namespace Futures
 {
     using System;
     using System.Collections.Generic;
     using System.Reactive.Disposables;
 
-    using Futures;
-
     public class TestFuture<T> : IFuture<T>
     {
         public TestFuture()
         {
-            Observers = new List<IFutureObserver<T>>();
+            this.Observers = new List<IFutureObserver<T>>();
         }
 
         public List<IFutureObserver<T>> Observers { get; private set; }
 
         public IDisposable Subscribe(IFutureObserver<T> observer)
         {
-            Observers.Add(observer);
-            return Disposable.Create(() => Observers.Remove(observer));
+            this.Observers.Add(observer);
+            return Disposable.Create(() => this.Observers.Remove(observer));
         }
 
         public void SetResult(T result)
         {
-            Array.ForEach(Observers.ToArray(), o => o.OnDone(result));
+            Array.ForEach(this.Observers.ToArray(), o => o.OnDone(result));
         }
 
         public void SetError(Exception error)
         {
-            Array.ForEach(Observers.ToArray(), o => o.OnError(error));
+            Array.ForEach(this.Observers.ToArray(), o => o.OnError(error));
         }
     }
 }
