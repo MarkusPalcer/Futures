35aa6c74ca0dd759bbb2957787924954a62d6c9a
pal <pal@zuehlke.com>
2015-03-11 13:45:22 +0100

    Added SubscribeOn and ObserveOn

---
+++ Futures/FutureTests/Scheduling.cs
@@ -0,0 +1,76 @@
+﻿namespace FutureTests
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Futures;
+
+    using Microsoft.Reactive.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Scheduling
+    {
+        [TestMethod]
+        public void ScheduleOn()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<int>();
+            var observer = new TestObserver<int>();
+
+            future.SubscribeOn(scheduler).Subscribe(observer);
+
+            future.Observers.Should().BeEmpty();
+
+            scheduler.AdvanceBy(1);
+
+            future.Observers.Should().HaveCount(1);
+        }
+
+        [TestMethod]
+        public void ObserveOnWithResult()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<int>();
+            var observer = new TestObserver<int>();
+
+            future.ObserveOn(scheduler).Subscribe(observer);
+
+            future.Observers.Should().HaveCount(1);
+
+            future.SetResult(42);
+
+            observer.Events.Should().BeEmpty();
+
+            scheduler.AdvanceBy(1);
+
+            observer.Events.Should().Equal(Notification.OnDone(42));
+
+            future.Observers.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void ObserveOnWithError()
+        {
+            var scheduler = new TestScheduler();
+            var future = new TestFuture<int>();
+            var observer = new TestObserver<int>();
+            var ex = new NotImplementedException();
+
+            future.ObserveOn(scheduler).Subscribe(observer);
+
+            future.Observers.Should().HaveCount(1);
+
+            future.SetError(ex);
+
+            observer.Events.Should().BeEmpty();
+
+            scheduler.AdvanceBy(1);
+
+            observer.Events.Should().Equal(Notification.OnError<int>(ex));
+
+            future.Observers.Should().BeEmpty();
+        }
+    }
+}
---
+++ Futures/Futures/Extensions/Scheduling.cs
@@ -0,0 +1,51 @@
+﻿namespace Futures
+{
+    using System.Reactive.Concurrency;
+    using System.Reactive.Disposables;
+
+    public partial class Future
+    {
+        /// <summary>
+        /// Causes the subscriptions to be executed on the specified scheduler
+        /// </summary>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="source">The future to modify</param>
+        /// <param name="scheduler">The scheduler to execute subscriptions on</param>
+        /// <returns>A future which executes <see cref="IFuture{T}#Subscribe"/> on the specified scheduler</returns>
+        public static IFuture<T> SubscribeOn<T>(this IFuture<T> source, IScheduler scheduler)
+        {
+            return Create<T>(
+                o =>
+                    {
+                        var scheduledAction = new MultipleAssignmentDisposable();
+                        var result = new CompositeDisposable(scheduledAction);
+
+                        scheduledAction.Disposable = scheduler.Schedule(() => result.Add(source.Subscribe(o)));
+
+                        return result;
+                    });
+        }
+
+        /// <summary>
+        /// Causes the result callbacks of the future to be executed on the specified scheduler
+        /// </summary>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="source">The future to modify</param>
+        /// <param name="scheduler">The scheduler to execute result callbacks on</param>
+        /// <returns>A future which executes <see cref="IFuture{T}#OnDone"/> and <see cref="IFuture{T}#OnError"/> on the specified scheduler</returns>
+        public static IFuture<T> ObserveOn<T>(this IFuture<T> source, IScheduler scheduler)
+        {
+            return Create<T>(
+                o =>
+                    {
+                        var result = new CompositeDisposable();
+                        result.Add(
+                            source.Subscribe(
+                                r => result.Add(scheduler.Schedule(() => o.OnDone(r))),
+                                ex => result.Add(scheduler.Schedule(() => o.OnError(ex)))));
+
+                        return result;
+                    });
+        } 
+    }
+}
